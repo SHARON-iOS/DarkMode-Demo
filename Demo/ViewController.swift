a3ed64547b3146542a9a1788874977f3fcab8c13
SHARON-iOS <sharon@sparkouttech.com>
2020-07-17 11:49:46 +0530

    first commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by SHARON D ROSE on 08/07/20.
+//  Copyright © 2020 SHARON D ROSE. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var namelble: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    @IBOutlet weak var firstVW: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+
+    }
+
+
+    
+    
+
+    
+}
+
